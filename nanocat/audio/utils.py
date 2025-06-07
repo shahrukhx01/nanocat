6d983312c1a9c072724c0469899811ae936ebf8d
Shahrukh Khan <sk28671@gmail.com>
2025-06-07 09:26:09 +0200

    misc: remove unnecessary utils (#10)

--- nanocat/audio/utils.py
+++ nanocat/audio/utils.py
@@ -4,8 +4,6 @@
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-import audioop
-
 import numpy as np
 import pyloudnorm as pyln
 import soxr
@@ -56,37 +54,6 @@ def create_default_resampler(**kwargs) -> BaseAudioResampler:
     return SOXRAudioResampler(**kwargs)
 
 
-def mix_audio(audio1: bytes, audio2: bytes) -> bytes:
-    data1 = np.frombuffer(audio1, dtype=np.int16)
-    data2 = np.frombuffer(audio2, dtype=np.int16)
-
-    # Max length
-    max_length = max(len(data1), len(data2))
-
-    # Zero-pad the arrays to the same length
-    padded1 = np.pad(data1, (0, max_length - len(data1)), mode="constant")
-    padded2 = np.pad(data2, (0, max_length - len(data2)), mode="constant")
-
-    # Mix the arrays
-    mixed_audio = padded1.astype(np.int32) + padded2.astype(np.int32)
-    mixed_audio = np.clip(mixed_audio, -32768, 32767).astype(np.int16)
-
-    return mixed_audio.astype(np.int16).tobytes()
-
-
-def interleave_stereo_audio(left_audio: bytes, right_audio: bytes) -> bytes:
-    left = np.frombuffer(left_audio, dtype=np.int16)
-    right = np.frombuffer(right_audio, dtype=np.int16)
-
-    min_length = min(len(left), len(right))
-    left = left[:min_length]
-    right = right[:min_length]
-
-    stereo = np.column_stack((left, right))
-
-    return stereo.astype(np.int16).tobytes()
-
-
 def normalize_value(value, min_value, max_value):
     normalized = (value - min_value) / (max_value - min_value)
     normalized_clamped = max(0, min(1, normalized))
@@ -110,51 +77,3 @@ def calculate_audio_volume(audio: bytes, sample_rate: int) -> float:
 
 def exp_smoothing(value: float, prev_value: float, factor: float) -> float:
     return prev_value + factor * (value - prev_value)
-
-
-async def ulaw_to_pcm(
-    ulaw_bytes: bytes, in_rate: int, out_rate: int, resampler: BaseAudioResampler
-):
-    # Convert μ-law to PCM
-    in_pcm_bytes = audioop.ulaw2lin(ulaw_bytes, 2)
-
-    # Resample
-    out_pcm_bytes = await resampler.resample(in_pcm_bytes, in_rate, out_rate)
-
-    return out_pcm_bytes
-
-
-async def pcm_to_ulaw(
-    pcm_bytes: bytes, in_rate: int, out_rate: int, resampler: BaseAudioResampler
-):
-    # Resample
-    in_pcm_bytes = await resampler.resample(pcm_bytes, in_rate, out_rate)
-
-    # Convert PCM to μ-law
-    out_ulaw_bytes = audioop.lin2ulaw(in_pcm_bytes, 2)
-
-    return out_ulaw_bytes
-
-
-async def alaw_to_pcm(
-    alaw_bytes: bytes, in_rate: int, out_rate: int, resampler: BaseAudioResampler
-) -> bytes:
-    # Convert a-law to PCM
-    in_pcm_bytes = audioop.alaw2lin(alaw_bytes, 2)
-
-    # Resample
-    out_pcm_bytes = await resampler.resample(in_pcm_bytes, in_rate, out_rate)
-
-    return out_pcm_bytes
-
-
-async def pcm_to_alaw(
-    pcm_bytes: bytes, in_rate: int, out_rate: int, resampler: BaseAudioResampler
-):
-    # Resample
-    in_pcm_bytes = await resampler.resample(pcm_bytes, in_rate, out_rate)
-
-    # Convert PCM to μ-law
-    out_alaw_bytes = audioop.lin2alaw(in_pcm_bytes, 2)
-
-    return out_alaw_bytes
